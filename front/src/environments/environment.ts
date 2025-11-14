f3d94d0f539484102cd0ae9d671175e98a79770f
Guilherme Monteiro <guihmonteiro93@gmail.com>
2025-11-14 14:48:31 +0000

    add-back-auth

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://congenial-doodle-g4w75jjj4vw73w9w9-3000.app.github.dev',
+  viaCepUrl: 'https://viacep.com.br/ws'
 };
 
 /*
